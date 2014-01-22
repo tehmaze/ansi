e44c16b128ee31703ca15e782ff89fd70812be94
Wijnand Modderman-Lenstra <maze@pyth0n.org>
2014-01-22 03:07:10 +0100

    Use ECMA-048 names as defaults.
    The old (incorrect) names are kept as an alias for backwards
    compatability. Note that "brown" was replaced by "yellow", so
    "yellow" no longer is "bold + brown".

--- ansi/colour/bg.py
+++ ansi/colour/bg.py
@@ -1,25 +1,42 @@
 #pylint: disable=C0103,R0903
 
-from ansi.sequence import sequence
 from ansi.colour.base import Graphic
-from ansi.colour.fx import bright
+from ansi.colour.fx import bold
 
+# ECMA-048 standard names
 black           = Graphic('40')
 red             = Graphic('41')
 green           = Graphic('42')
-brown           = Graphic('43')
+yellow          = Graphic('43')
 blue            = Graphic('44')
 magenta         = Graphic('45')
 cyan            = Graphic('46')
-grey            = Graphic('47')
-gray            = grey                  # US English
+white           = Graphic('47')
 default         = Graphic('49')
-darkgrey        = bright + black
-darkgray        = darkgrey              # US English
-brightred       = bright + red
-brightgreen     = bright + green
-yellow          = bright + brown
-brightblue      = bright + blue
-brightmagenta   = bright + magenta
-brightcyan      = bright + cyan
-white           = bright + grey
+
+# ECMA-048 bold variants
+boldblack       = bold + black
+boldred         = bold + red
+boldgreen       = bold + green
+boldyellow      = bold + yellow
+boldblue        = bold + blue
+boldmagenta     = bold + magenta
+boldcyan        = bold + cyan
+boldwhite       = bold + white
+
+# Convenience wrappers
+brown           = yellow                # Not in ANSI/ECMA-048 standard
+grey            = white                 # Not in ANSI/ECMA-048 standard
+gray            = white                 # US English
+darkgrey        = boldblack
+darkgray        = boldblack             # US English
+brightred       = boldred
+brightgreen     = boldgreen
+brightyellow    = boldyellow
+brightbrown     = boldyellow            # Not in ANSI/ECMA-048 standard
+brightblue      = boldblue
+brightmagenta   = boldmagenta
+brightcyan      = boldcyan
+brightwhite     = boldwhite
+brightgrey      = boldwhite             # Not in ANSI/ECMA-048 standard
+brightgray      = boldwhite             # Us English
--- ansi/colour/fg.py
+++ ansi/colour/fg.py
@@ -1,26 +1,43 @@
 #pylint: disable=C0103,R0903
 
-from ansi.sequence import sequence
 from ansi.colour.base import Graphic
-from ansi.colour.fx import bright
+from ansi.colour.fx import bold
 
 
+# ECMA-048 standard names
 black           = Graphic('30')
 red             = Graphic('31')
 green           = Graphic('32')
-brown           = Graphic('33')
-blue            = Graphic('33')
+yellow          = Graphic('33')
+blue            = Graphic('34')
 magenta         = Graphic('35')
 cyan            = Graphic('36')
-grey            = Graphic('37')
-gray            = grey                  # US English
+white           = Graphic('37')
 default         = Graphic('39')
-darkgrey        = bright + black
-darkgray        = darkgrey              # US English
-brightred       = bright + red
-brightgreen     = bright + green
-yellow          = bright + brown
-brightblue      = bright + blue
-brightmagenta   = bright + magenta
-brightcyan      = bright + cyan
-white           = bright + grey
+
+# ECMA-048 bold variants
+boldblack       = bold + black
+boldred         = bold + red
+boldgreen       = bold + green
+boldyellow      = bold + yellow
+boldblue        = bold + blue
+boldmagenta     = bold + magenta
+boldcyan        = bold + cyan
+boldwhite       = bold + white
+
+# Convenience wrappers
+brown           = yellow                # Not in ANSI/ECMA-048 standard
+grey            = white                 # Not in ANSI/ECMA-048 standard
+gray            = white                 # US English
+darkgrey        = boldblack
+darkgray        = boldblack             # US English
+brightred       = boldred
+brightgreen     = boldgreen
+brightyellow    = boldyellow
+brightbrown     = boldyellow            # Not in ANSI/ECMA-048 standard
+brightblue      = boldblue
+brightmagenta   = boldmagenta
+brightcyan      = boldcyan
+brightwhite     = boldwhite
+brightgrey      = boldwhite             # Not in ANSI/ECMA-048 standard
+brightgray      = boldwhite             # Us English
