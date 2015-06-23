01f28edb29c64c8ba324d127f1f25074d531a0bf
Markus Unterwaditzer <markus@unterwaditzer.net>
2015-06-23 11:15:53 +0200

    Fix tests
    Printing the expected result gives me a bold string when in fact it
    shouldn't be.
    
    The actual result, however, seems to look as expected.

--- test_ansi.py
+++ test_ansi.py
@@ -13,12 +13,12 @@ def test_fg_bg():
     from ansi.colour import fg, bg
     from ansi.colour.fx import reset
     msg = (bg.red, fg.yellow, 'Hello world!', reset)
-    assert ''.join(map(str, msg)) == '\x1b[41m\x1b[1;33mHello world!\x1b[0m'
+    assert ''.join(map(str, msg)) == '\x1b[41m\x1b[33mHello world!\x1b[0m'
 
 def test_sugar():
     from ansi.colour import fg, bg
     assert bg.red(fg.yellow('Hello world!')) == \
-        '\x1b[41m\x1b[1;33mHello world!\x1b[0m\x1b[0m'
+        '\x1b[41m\x1b[33mHello world!\x1b[0m\x1b[0m'
 
 def test_rgb():
     from ansi.colour.rgb import rgb256
