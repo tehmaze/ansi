05d868deaf532d59e97a18266e72980958fb4356
Jeff Quast <contact@jeffquast.com>
2022-01-29 18:09:16 -0500

    LGTM complaint

--- ansi/color.py
+++ ansi/color.py
@@ -1,7 +1,7 @@
 # pylint: disable=C0111,W0611,W0614
 from sys import modules
 
-import ansi.colour
+import ansi.colour  # lgtm [py/import-and-import-from]
 from ansi.colour import bg, fg, fx, rgb
 
 modules["ansi.color"] = ansi.colour
